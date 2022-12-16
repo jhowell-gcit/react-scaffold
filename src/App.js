1f6fca09533cb0b754321a24a1ea60b7d7a8cc0a
Jordan <Jordan.Howell@gwinnettcounty.com>
2022-12-16 16:23:26 -0500

    init simple form to communicate with node api scaffold GET func

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [code, setCode] = useState("");
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+  const [resErr, setResErr] = useState("");
+
+  let handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let res = await fetch("/api/v1/sample-path?" + (code ? "sampleCode=" + code : "") + (name ? "&sampleName=" + name : ""), {
+        method: "GET",
+      })
+      let resJson = await res.json();
+      if (res.status === 200) {
+        setCode("");
+        setName("");
+        setResErr(false)
+      } else {
+        setResErr(true)
+      }
+      setMessage(JSON.stringify(resJson, null, 2));
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="sampleCode"
+          value={code}
+          onChange={(e) => setCode(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="sampleName"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+
+        <button type="submit">GET</button>
+
+        <div className="message" style={resErr ? { color:"red" } : { color:"green" }}>{message ? <pre>{message}</pre> : null}</div>
+      </form>
     </div>
   );
 }
